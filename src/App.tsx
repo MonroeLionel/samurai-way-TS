7ff50706a633e3df4be65c255c6925efd3ddb1cb
MonroeLionel <MonroeLionel@gmail.com>
2022-06-25 13:29:01 +0300

    11. Уроки React JS (соц. сеть, шаг 1, вёрстка, grid)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,34 @@ import React from 'react';
 import './App.css';
 import {Technologies} from "./Technologies";
 import {Header} from "./Header";
+import {Footer} from "./Footer";
 
 function App() {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://thumbs.dreamstime.com/b/phoenix-bird-fire-vector-colorful-icon-abstract-logo-design-bright-gradient-colors-89986014.jpg"
+                    alt=""/>
+            </header>
+            <nav className="nav">
+                <div><a href="">profile</a></div>
+                <div><a href="">message</a></div>
+                <div><a href="">news</a></div>
+                <div><a href="">music</a></div>
+                <div><a href="">setting</a></div>
+
+            </nav>
+            <div className="content"><img
+                src="https://img2.akspic.ru/crops/8/5/6/8/6/168658/168658-fraktalnoe_iskusstvo-art-risovanie-purpur-naturalnyj_material-3840x2160.jpg"
+                alt=""/>
+                <div>ава+описание</div>
+                <div>посты</div>
+                <div>новые посты</div>
+                <div>пост1</div>
+                <div>пост 1</div>
+            </div>
+
         </div>
     );
 }
