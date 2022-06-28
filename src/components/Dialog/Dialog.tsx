e19d42333e4d91f82589498058381203f3b06e28
MonroeLionel <MonroeLionel@gmail.com>
2022-06-28 20:09:15 +0300

    21. Уроки React JS (Верстаем страницу Диалогов) - react курсы бесплатно

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -4,8 +4,21 @@ import classes from "./Dialog.module.css"
 
 export function Dialog() {
    return (
-     <div>
-        dialog
+     <div className={classes.dialogs}>
+        <div className={classes.dialogItem}>
+           <div className={classes.dialog + ' ' + classes.active}>света</div>
+           <div className={classes.dialog}>дима</div>
+           <div className={classes.dialog}>антоша</div>
+           <div className={classes.dialog}>катюша</div>
+           <div className={classes.dialog}>павлуша</div>
+           <div className={classes.dialog}>андрюша</div>
+        </div>
+        <div className={classes.messages}>
+           <div className={classes.message}>текст</div>
+           <div className={classes.message}>еще текст</div>
+           <div className={classes.message}>типичная запись</div>
+        </div>
      </div>
    )
 }
+
