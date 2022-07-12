2c56c1ce75e415be634881ed22d66a3f638bc519
MonroeLionel <MonroeLionel@gmail.com>
2022-07-12 18:45:55 +0300

    31. Уроки React JS - onClick, ref, VirtualDOM

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {LegacyRef} from "react";
 import classes from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 import {postDataType} from "../../../App";
@@ -12,12 +12,27 @@ export function MyPosts(props: postDataPropsTpe) {
    let postsElements = props.postData.map((post) => {
       return <Post message={post.message} likeCount={post.likeCount} id={post.id}/>
    })
+
+   let newPostElement = React.createRef<HTMLTextAreaElement>()
+
+   const addPost = () => {
+      let text = newPostElement.current
+      if (text) {
+         console.log(text.value)
+         alert(text.value)
+      }
+
+
+   }
+
    return (
      <div className={classes.postsBlock}>
         <div><h3>мои посты</h3></div>
-        <div><textarea></textarea></div>
+        <div><textarea ref={newPostElement}></textarea></div>
         <div>
-           <button>добавить пост</button>
+           <button onClick={addPost
+           }>добавить пост
+           </button>
         </div>
         <div className={classes.post}>
            {postsElements}
